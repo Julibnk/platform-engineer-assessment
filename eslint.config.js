f86af3e6ee4e8a851416f5a1c28065c33c9c1662
julibnk <fernandezfernandezjulian@gmail.com>
2026-05-20 07:46:31 +0200

    Init pnpm monorepo

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: true },
+      globals: { ...globals.node, ...globals.browser },
+    },
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  { ignores: ["**/dist/**", "**/node_modules/**", "**/.next/**"] },
+];
